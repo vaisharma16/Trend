f8cc3dfb25db823ae4e90d1b21225414fc59ff28
Vaibhav Sharma <vaisharma16@gmail.com>
2020-12-01 00:17:02 +0530

    done

--- shop/settings.py
+++ shop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'nk)tdywa&+w$6vsjl(^7fa8_1c(t4*c&+g(e0_y&v7cc^p0g7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['trend-in-one.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['trend-in-one.herokuapp.com', '127.0.0.1' ]
 
 
 # Application definition
